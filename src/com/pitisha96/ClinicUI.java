b87f7ef6716cb596afc2f86b81ec60311f1d68fc
Pitisha96 <petrikin1996@gmail.com>
2020-12-23 14:16:53 +0300

    added java-doc to Interface ClinicUI

--- src/com/pitisha96/ClinicUI.java
+++ src/com/pitisha96/ClinicUI.java
@@ -6,6 +6,15 @@ package com.pitisha96;
  * @since 20.12.2020
  */
 public interface ClinicUI {
+
+    /**
+     * Добавление нового действия в клинику
+     * @param action новое действие
+     */
     void loadActions(Action action);
+
+    /**
+     * Вывод меню клиники
+     */
     void show();
 }
