311cf394bc8b5046912e75bada595a4d34b1dbf4
Pitisha96 <petrikin1996@gmail.com>
2020-12-23 15:32:44 +0300

    created new Clinic Runner

--- src/com/pitisha96/Main.java
+++ src/com/pitisha96/Main.java
@@ -1,8 +1,20 @@
 package com.pitisha96;
 
+import com.pitisha96.Actions.CreateClientAction;
+import com.pitisha96.Actions.ExitFromClinic;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Validator validator=new Validator(new Scanner(System.in));
+        SingleClinicUI clinic=new SingleClinicUI(new Clinic(),validator);
+        clinic.loadActions(new CreateClientAction());
+        clinic.loadActions(new ExitFromClinic());
+        do{
+            clinic.show();
+            clinic.doAction(validator);
+        }while(true);
     }
 }
