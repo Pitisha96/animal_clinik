d517d8c1b8934dc0d07ff4805a1090b09a1b610e
Pitisha96 <petrikin1996@gmail.com>
2020-12-24 14:59:09 +0300

    created new class UpdatePetAction

---
+++ src/com/pitisha96/Actions/UpdatePetAction.java
@@ -0,0 +1,45 @@
+package com.pitisha96.Actions;
+
+import com.pitisha96.Action;
+import com.pitisha96.IClinic;
+import com.pitisha96.Validator;
+
+/**
+ * Переименование питомца
+ * @author Илья Петровский
+ * @since 24.12.2020
+ */
+public class UpdatePetAction implements Action {
+
+    /**
+     * {@inheritDoc}
+     * @param clinic  Клиника
+     * @param validator Валидатор ввода
+     */
+    @Override
+    public void execute(IClinic clinic, Validator validator) {
+        clinic.updatePet(
+                validator.getString("Имя хозяина питомца: "),
+                validator.getString("Имя питомца: "),
+                validator.getString("Новое имя питомца: ")
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return пункт меню
+     */
+    @Override
+    public String intro() {
+        return String.format("%s-переименовать питомца",this.key());
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return номер операции
+     */
+    @Override
+    public int key() {
+        return 4;
+    }
+}
