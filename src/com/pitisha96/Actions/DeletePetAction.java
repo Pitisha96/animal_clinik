b2f22e2bc9b2fd3d2f71544aaf923cdd909bd16f
Pitisha96 <petrikin1996@gmail.com>
2020-12-24 16:56:51 +0300

    created new class DeletePetAction

---
+++ src/com/pitisha96/Actions/DeletePetAction.java
@@ -0,0 +1,42 @@
+package com.pitisha96.Actions;
+
+import com.pitisha96.Action;
+import com.pitisha96.IClinic;
+import com.pitisha96.Validator;
+
+/**
+ * Удаление питомца
+ * @author Илья Петровский
+ * @since 24.12.2020
+ */
+public class DeletePetAction implements Action {
+
+    /**
+     * {@inheritDoc}
+     * @param clinic  Клиника
+     * @param validator Валидатор ввода
+     */
+    @Override
+    public void execute(IClinic clinic, Validator validator) {
+        clinic.deletePet(validator.getString("Имя клиента: "),
+                validator.getString("Имя питомца: "));
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return пункт меню
+     */
+    @Override
+    public String intro() {
+        return String.format("%s-удалить питомца",this.key());
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return номер операции
+     */
+    @Override
+    public int key() {
+        return 6;
+    }
+}
