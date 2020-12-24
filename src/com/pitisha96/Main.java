e60996d1f04ccc745b21875456364024ebfed8d3
Pitisha96 <petrikin1996@gmail.com>
2020-12-24 17:04:03 +0300

    added Actions and Exceptions

--- src/com/pitisha96/Main.java
+++ src/com/pitisha96/Main.java
@@ -1,20 +1,34 @@
 package com.pitisha96;
 
-import com.pitisha96.Actions.CreateClientAction;
-import com.pitisha96.Actions.ExitFromClinic;
+import com.pitisha96.Actions.*;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Validator validator=new Validator(new Scanner(System.in));
-        SingleClinicUI clinic=new SingleClinicUI(new Clinic(),validator);
-        clinic.loadActions(new CreateClientAction());
-        clinic.loadActions(new ExitFromClinic());
-        do{
-            clinic.show();
-            clinic.doAction(validator);
-        }while(true);
+        try (Validator validator=new Validator(new Scanner(System.in))){
+            SingleClinicUI clinic=new SingleClinicUI(new Clinic(),validator);
+            clinic.loadActions(new CreateClientAction());
+            clinic.loadActions(new ExitFromClinic());
+            clinic.loadActions(new CreatePetAction());
+            clinic.loadActions(new UpdateClientAction());
+            clinic.loadActions(new UpdatePetAction());
+            clinic.loadActions(new searchClientAction());
+            clinic.loadActions(new deleteClientAction());
+            clinic.loadActions(new DeletePetAction());
+            clinic.loadActions(new SearchPetAction());
+            do{
+                try {
+                    clinic.show();
+                    clinic.doAction(validator);
+                }catch (UnsupportedOperationException e){
+                    System.out.println("такая операция не поддерживается");
+                }catch (IllegalArgumentException e){
+                    validator.getString("");
+                    System.out.println("вы ввели не корректные данные");
+                }
+            }while(true);
+        }
     }
 }
